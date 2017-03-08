3b6bc8b566a98728ac474240c0f1d5b41de4f5ce
etiennedilocker <etienne@dilocker.de>
2017-03-08 22:33:16 +0100

    initial commit

---
+++ spec/atom-coverage-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomCoverageView from '../lib/atom-coverage-view';
+
+describe('AtomCoverageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
